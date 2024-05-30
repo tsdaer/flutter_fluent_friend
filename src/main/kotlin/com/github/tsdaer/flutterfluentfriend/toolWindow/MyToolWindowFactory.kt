4456da86c599ca6be0bc547d237062342284b4bc
GitHub Action <action@github.com>
2024-05-30 14:16:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/tsdaer/flutterfluentfriend/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/tsdaer/flutterfluentfriend/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.tsdaer.flutterfluentfriend.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.tsdaer.flutterfluentfriend.MyBundle
+import com.github.tsdaer.flutterfluentfriend.services.MyProjectService
 import javax.swing.JButton
 
 
